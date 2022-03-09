e29e2adb17ecc8e43a8d1a900040f2f39beaa69c
SWE-Jahidul <programmerjahid162@gmail.com>
2022-03-10 01:46:54 +0600

    category add

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-
-const cookieSession = require("cookie-session")
+const cookieSession = require("cookie-session");
 const express = require("express");
 const app = express();
 
 const mongoose = require("mongoose");
 const passport = require("passport");
-
 
 const passportSetup = require("./passport");
 const axios = require("axios");
@@ -17,43 +15,31 @@ const { stringify } = require("nodemon/lib/utils");
 
 app.set("port", process.env.PORT || 5000);
 
-
-
-app.use (
+app.use(
   cookieSession({
-    name:'session',
-    keys:['jahid'],
-    maxAge:24*60*60*100
+    name: "session",
+    keys: ["jahid"],
+    maxAge: 24 * 60 * 60 * 100,
   })
-)
+);
 
 app.use(cors());
 // parse application/json
 
 app.use(bodyParser.json());
 
-
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-
-
 const authRoute = require("./routes/auth");
 
-app.use(cors({
-  
-  
-  origin: "http://localhost:3006", 
-  methods:"GET,POST,PUT,DELETE",
-  credentials: true,
-
-
-
-}));
-
-
-
+app.use(
+  cors({
+    origin: "http://localhost:3006",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 
 //  MonngoDb Connect
 mongoose.connect(
@@ -100,86 +86,136 @@ async function run() {
     },
   });
 
+  // catagory Schema
+  const categorySchema = new mongoose.Schema({
+    category_name: {
+      type: String,
+    },
+    category_added_date: {
+      type: Date,
+      default: Date.now,
+    },
+  });
+
   // product Schema
 
   // const User = new mongoose.model("User", userSchema);
   const Products = new mongoose.model("Products", productSchema);
 
+  // category Schema
+  const Category = new mongoose.model("Category", categorySchema);
+
   // Post product Data
-  app.post(
-    "/products",
-    (req, res) => {
-      const {
-        product_name,
-        product_id,
-        stock_quality,
-        stock_qantity,
-        min_price,
-        product_brand,
-        category,
-        product_details,
-        added_date,
-        thambnil,
-        img,
-      } = req.body;
-      Products.findOne(
-        {
-          product_name: product_name,
-          product_id: product_id,
-          stock_quality: stock_quality,
-          stock_qantity: stock_qantity,
-          min_price: min_price,
-          product_brand: product_brand,
-          category: category,
-          product_details: product_details,
-          added_date: added_date,
-          thambnil: thambnil,
-          img: img,
-        },
-        (err, user) => {
-          const product11 = new Products({
-            product_name,
-            product_id,
-            stock_quality,
-            stock_qantity,
-            min_price,
-            product_brand,
-            category,
-            product_details,
-            added_date,
-            thambnil,
-            img,
-          });
-          product11.save((err) => {
-            if (err) {
-              res.send(err);
-              console.log("rrr", res);
-            } else {
-              res.send({
-                message: "Successfully Product Added , Please login now.",
-              });
-            }
-          });
+  app.post("/products", (req, res) => {
+    const {
+      product_name,
+      product_id,
+      stock_quality,
+      stock_qantity,
+      min_price,
+      product_brand,
+      category,
+      product_details,
+      added_date,
+      thambnil,
+      img,
+    } = req.body;
+    Products.findOne(
+      {
+        product_name: product_name,
+        product_id: product_id,
+        stock_quality: stock_quality,
+        stock_qantity: stock_qantity,
+        min_price: min_price,
+        product_brand: product_brand,
+        category: category,
+        product_details: product_details,
+        added_date: added_date,
+        thambnil: thambnil,
+        img: img,
+      },
+      (err, user) => {
+        const product11 = new Products({
+          product_name,
+          product_id,
+          stock_quality,
+          stock_qantity,
+          min_price,
+          product_brand,
+          category,
+          product_details,
+          added_date,
+          thambnil,
+          img,
+        });
+        product11.save((err) => {
+          if (err) {
+            res.send(err);
+            console.log("rrr", res);
+          } else {
+            res.send({
+              message: "Successfully Product Added , Please login now.",
+            });
+          }
+        });
+      }
+    );
+  });
+
+  // Post Category
+  app.post("/category", (req, res) => {
+    const { category_name, category_added_date } = req.body;
+
+    Category.findOne({
+      category_name: category_name,
+      category_added_date: category_added_date,
+    },
+    (err, user) => {
+
+      const Category11 = new Category ({
+        category_name,
+        category_added_date
+      });
+      Category11.save((err) =>{
+        if(err){
+          res.send(err)
+          console.log(res);
+
         }
-      );
+        else{
+          res.send({
+            message: "Successfully Category Added",
+
+          })
+        }
+      })
     }
-
-    
-  );
-
+    );
+  });
+ 
   // Product get Method
   app.get("/products", async (req, res) => {
     const getProduct = await Products.find({});
 
     res.send(getProduct);
   });
+
+// Category get 
+
+app.get("/category" , async(req , res ) => {
+  const getCategory = await Category.find({});
+  res.send(getCategory)
+})
+
 }
+
+
+
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
-
 
 app.use("/auth", authRoute);
 
